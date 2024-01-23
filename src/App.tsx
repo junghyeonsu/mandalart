d3a3d67fd2fdc7d6334db831bffa34493ccad572
정현수 <54893898+junghyeonsu@users.noreply.github.com>
2024-01-24 00:10:45 +0900

    feat: remove react-ga, install vite-plugin-radar

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
-import ga from "react-ga";
-
 import { Mandalart } from "./components/Mandalart";
 import MandalartProvider from "./contexts/MandalartContext";
 
-ga.initialize("G-7S52Y1P368");
-
 function App() {
   return (
-    <div className="flex items-center justify-center w-full h-dvh">
+    <div className="flex items-center justify-center w-full h-dvh h-vh">
       <MandalartProvider>
         <Mandalart />
       </MandalartProvider>
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,20 @@
 import react from "@vitejs/plugin-react";
 import path from "path";
 import { defineConfig } from "vite";
+import { VitePluginRadar } from "vite-plugin-radar";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePluginRadar({
+      analytics: {
+        id: "G-7S52Y1P368",
+        config: {
+          send_page_view: true,
+        },
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
