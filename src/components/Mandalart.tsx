398da806a0daae64c3c1eddb5bb39a61ba4c9a2f
정현수 <54893898+junghyeonsu@users.noreply.github.com>
2024-02-03 00:08:37 +0900

    feat: reactflow data로 교체

--- src/components/Mandalart.tsx
+++ src/components/Mandalart.tsx
@@ -1,163 +1,49 @@
-/* eslint-disable no-unused-vars */
 import "reactflow/dist/style.css";
 
 import { RotateCcwIcon } from "lucide-react";
-import { memo, useCallback, useMemo, useState } from "react";
-import { useMediaQuery } from "react-responsive";
-import type { Edge, Node, OnEdgesChange, OnNodesChange } from "reactflow";
-import ReactFlow, { applyEdgeChanges, applyNodeChanges, Background, Controls, MiniMap } from "reactflow";
+import { memo } from "react";
+import ReactFlow, { Background, Controls, MiniMap } from "reactflow";
 
-import type { MandalartItem, MandalartSection } from "@/contexts/MandalartContext";
-import { Position, useMandalartDispatch, useMandalartState } from "@/contexts/MandalartContext";
+import { type NodeData, useMandalartDispatch, useMandalartState } from "@/contexts/MandalartContext";
 
-import { Dialog, DialogContent, DialogTrigger } from "./ui/dialog";
 import { Textarea } from "./ui/textarea";
 
-const centerNodes: Node[] = [
-  { id: "A", type: "group", position: { x: 100, y: 100 }, data: { label: null }, style: { width: 300, height: 500 } },
-  { id: "B", type: "input", extent: "parent", position: { x: 80, y: 100 }, data: { label: "1" }, parentNode: "A" },
-  { id: "C", type: "input", extent: "parent", position: { x: 80, y: 200 }, data: { label: "2" }, parentNode: "A" },
-  { id: "D", type: "input", extent: "parent", position: { x: 80, y: 300 }, data: { label: "3" }, parentNode: "A" },
-  { id: "E", type: "input", extent: "parent", position: { x: 80, y: 400 }, data: { label: "4" }, parentNode: "A" },
-];
+const CustomTextNode = ({ data }: { data: NodeData }) => {
+  const { changeTitle } = useMandalartDispatch();
 
-const initialNodes: Node[] = [...centerNodes];
-const initialEdges: Edge[] = [{ id: "eB-C", source: "B", target: "C" }];
+  const { id, title } = data;
+
+  const onChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    changeTitle(id, e.target.value);
+  };
+
+  return <Textarea value={title} onChange={onChange} className={`resize-none text-center`} />;
+};
+
+const nodeTypes = { textUpdater: CustomTextNode };
 
 const Mandalart = () => {
-  const [nodes, setNodes] = useState<Node[]>(initialNodes);
-  const [edges, setEdges] = useState<Edge[]>(initialEdges);
-
-  const isLarge = useMediaQuery({ query: "(min-width: 1024px)" });
-  const { mandalartData } = useMandalartState();
-
-  const onNodesChange: OnNodesChange = useCallback(
-    (changes) => setNodes((nds) => applyNodeChanges(changes, nds)),
-    [setNodes],
-  );
-  const onEdgesChange: OnEdgesChange = useCallback(
-    (changes) => setEdges((eds) => applyEdgeChanges(changes, eds)),
-    [setEdges],
-  );
+  const { nodes } = useMandalartState();
+  const { onNodesChange, setRfInstance } = useMandalartDispatch();
 
   return (
     <>
       <ResetButton />
 
-      <div className="w-[100vh] h-[100vh] border-4 border-rose-500">
-        <ReactFlow fitView nodes={nodes} edges={edges} onNodesChange={onNodesChange} onEdgesChange={onEdgesChange}>
+      <div className="w-[100vh] h-[100vh] border-2 border-rose-500">
+        <ReactFlow fitView nodes={nodes} onNodesChange={onNodesChange} nodeTypes={nodeTypes} onInit={setRfInstance}>
           <Controls />
           <MiniMap />
           <Background gap={20} size={1} />
         </ReactFlow>
       </div>
-
-      {/* <div className="grid grid-cols-3 gap-2 lg:gap-4">
-        {mandalartData.map((section, sectionPosition) => {
-          return isLarge ? (
-            <Section key={sectionPosition} sectionData={section} sectionPosition={sectionPosition} modal={false} />
-          ) : (
-            <Dialog key={sectionPosition}>
-              <DialogTrigger>
-                <Section sectionData={section} sectionPosition={sectionPosition} />
-              </DialogTrigger>
-              <DialogContent className="flex flex-col items-center">
-                <ModalPreviewSection position={sectionPosition} />
-                <Section modal sectionData={section} sectionPosition={sectionPosition} />
-              </DialogContent>
-            </Dialog>
-          );
-        })}
-      </div> */}
     </>
   );
 };
 
 const ResetButton = memo(() => {
-  const { resetMandalartData } = useMandalartDispatch();
-  return (
-    <RotateCcwIcon onClick={resetMandalartData} className="fixed top-4 right-4 w-6 h-6 text-primary cursor-pointer" />
-  );
-});
-
-const ModalPreviewSection = ({ position }: { position: Position }) => {
-  return (
-    <div className="grid grid-cols-3 gap-1 w-16 h-16">
-      <div className={`w-4 h-4 ${position === Position.topLeft ? "bg-primary" : "bg-gray-300"}`} />
-      <div className={`w-4 h-4 ${position === Position.topCenter ? "bg-primary" : "bg-gray-300"}`} />
-      <div className={`w-4 h-4 ${position === Position.topRight ? "bg-primary" : "bg-gray-300"}`} />
-      <div className={`w-4 h-4 ${position === Position.centerLeft ? "bg-primary" : "bg-gray-300"}`} />
-      <div className={`w-4 h-4 ${position === Position.centerCenter ? "bg-primary" : "bg-gray-300"}`} />
-      <div className={`w-4 h-4 ${position === Position.centerRight ? "bg-primary" : "bg-gray-300"}`} />
-      <div className={`w-4 h-4 ${position === Position.bottomLeft ? "bg-primary" : "bg-gray-300"}`} />
-      <div className={`w-4 h-4 ${position === Position.bottomCenter ? "bg-primary" : "bg-gray-300"}`} />
-      <div className={`w-4 h-4 ${position === Position.bottomRight ? "bg-primary" : "bg-gray-300"}`} />
-    </div>
-  );
-};
-
-interface SectionProps {
-  sectionData: MandalartSection;
-  sectionPosition: Position;
-  modal?: boolean;
-}
-
-const Section = ({ sectionData, sectionPosition, modal }: SectionProps) => {
-  return (
-    <div className="grid grid-cols-3 gap-1 lg:gap-2 items-center">
-      {sectionData.data.map((item, itemPosition) => {
-        return (
-          <Item
-            key={`${sectionPosition}+${itemPosition}`}
-            item={item}
-            itemPosition={itemPosition}
-            sectionPosition={sectionPosition}
-            modal={modal}
-          />
-        );
-      })}
-    </div>
-  );
-};
-
-interface ItemProps {
-  item: MandalartItem;
-  itemPosition: Position;
-  sectionPosition: Position;
-  modal?: boolean;
-}
-
-const Item = memo(({ item, itemPosition, sectionPosition, modal }: ItemProps) => {
-  const { changeMandalartItemTitle } = useMandalartDispatch();
-
-  const isCenterItem = useMemo(() => itemPosition === Position.centerCenter, [itemPosition]);
-  const isCenterSection = useMemo(() => sectionPosition === Position.centerCenter, [sectionPosition]);
-  const isOutsideSectionCenter = useMemo(() => isCenterItem && !isCenterSection, [isCenterItem, isCenterSection]);
-  const title = useMemo(() => item.title, [item.title]);
-
-  const handleChange = useCallback(
-    (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-      changeMandalartItemTitle(sectionPosition, itemPosition, e.target.value);
-
-      if (isCenterSection) {
-        changeMandalartItemTitle(itemPosition, sectionPosition, e.target.value);
-      }
-    },
-    [changeMandalartItemTitle, isCenterSection, itemPosition, sectionPosition],
-  );
-
-  return (
-    <div key={itemPosition} className={`lg:w-20 lg:h-20 ${modal ? "w-24 h-24" : "w-10 h-10"}`}>
-      <Textarea
-        className={`resize-none text-xxs lg:text-sm text-center ${isCenterItem && "font-bold"} ${
-          isOutsideSectionCenter && "bg-gray-100"
-        }`}
-        value={title}
-        onChange={handleChange}
-        readOnly={isOutsideSectionCenter}
-      />
-    </div>
-  );
+  const { resetNodes } = useMandalartDispatch();
+  return <RotateCcwIcon onClick={resetNodes} className="fixed top-4 right-4 w-6 h-6 text-primary cursor-pointer" />;
 });
 
 export { Mandalart };
--- src/contexts/MandalartContext.tsx
+++ src/contexts/MandalartContext.tsx
@@ -1,220 +1,155 @@
 /* eslint-disable react-refresh/only-export-components */
 /* eslint-disable no-unused-vars */
+import { useDebounce } from "@toss/react";
 import { produce } from "immer";
 import { createContext, useCallback, useContext, useEffect, useState } from "react";
+import type { OnNodesChange, ReactFlowInstance, ReactFlowJsonObject } from "reactflow";
+import { type Node, useNodesState } from "reactflow";
 
-export const MANDAL_ART_KEY = "mandalart";
-
-export enum Position {
-  topLeft,
-  topCenter,
-  topRight,
-  centerLeft,
-  centerCenter,
-  centerRight,
-  bottomLeft,
-  bottomCenter,
-  bottomRight,
+interface MandalartContextState {
+  nodes: Node[];
 }
 
-export interface MandalartItem {
-  title: string;
-}
+interface MandalartContextDispatch {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  setRfInstance: (instance: any) => void;
 
-export interface MandalartSection {
-  position: Position;
-  data: MandalartItem[];
-}
+  onNodesChange: OnNodesChange;
 
-export interface MandalartContextState {
-  mandalartData: MandalartSection[];
-}
+  save: () => void;
 
-export interface MandalartContextDispatch {
-  changeMandalartItemTitle: (sectionIndex: number, itemIndex: number, title: string) => void;
-  resetMandalartData: () => void;
+  resetNodes: () => void;
+
+  changeTitle: (id: string, title: string) => void;
 }
 
 const MandalartStateContext = createContext<MandalartContextState | undefined>(undefined);
 const MandalartDispatchContext = createContext<MandalartContextDispatch | undefined>(undefined);
 
-const initialData: MandalartSection[] = [
-  {
-    position: Position.topLeft,
-    data: [
-      { title: "" },
-      { title: "" },
-      { title: "" },
-      { title: "" },
-      { title: "" },
-      { title: "" },
-      { title: "" },
-      { title: "" },
-      { title: "" },
-    ],
-  },
-  {
-    position: Position.topCenter,
-    data: [
-      { title: "" },
-      { title: "" },
-      { title: "" },
-      { title: "" },
-      { title: "" },
-      { title: "" },
-      { title: "" },
-      { title: "" },
-      { title: "" },
-    ],
-  },
-  {
-    position: Position.topRight,
-    data: [
-      { title: "" },
-      { title: "" },
-      { title: "" },
-      { title: "" },
-      { title: "" },
-      { title: "" },
-      { title: "" },
-      { title: "" },
-      { title: "" },
-    ],
-  },
-  {
-    position: Position.centerLeft,
-    data: [
-      { title: "" },
-      { title: "" },
-      { title: "" },
-      { title: "" },
-      { title: "" },
-      { title: "" },
-      { title: "" },
-      { title: "" },
-      { title: "" },
-    ],
-  },
-  {
-    position: Position.centerCenter,
-    data: [
-      { title: "" },
-      { title: "" },
-      { title: "" },
-      { title: "" },
-      { title: "" },
-      { title: "" },
-      { title: "" },
-      { title: "" },
-      { title: "" },
-    ],
-  },
-  {
-    position: Position.centerRight,
-    data: [
-      { title: "" },
-      { title: "" },
-      { title: "" },
-      { title: "" },
-      { title: "" },
-      { title: "" },
-      { title: "" },
-      { title: "" },
-      { title: "" },
-    ],
-  },
-  {
-    position: Position.bottomLeft,
-    data: [
-      { title: "" },
-      { title: "" },
-      { title: "" },
-      { title: "" },
-      { title: "" },
-      { title: "" },
-      { title: "" },
-      { title: "" },
-      { title: "" },
-    ],
-  },
-  {
-    position: Position.bottomCenter,
-    data: [
-      { title: "" },
-      { title: "" },
-      { title: "" },
-      { title: "" },
-      { title: "" },
-      { title: "" },
-      { title: "" },
-      { title: "" },
-      { title: "" },
-    ],
-  },
-  {
-    position: Position.bottomRight,
-    data: [
-      { title: "" },
-      { title: "" },
-      { title: "" },
-      { title: "" },
-      { title: "" },
-      { title: "" },
-      { title: "" },
-      { title: "" },
-      { title: "" },
-    ],
-  },
+const FLOW_KEY = "mandalart-flow";
+
+const GROUP_SIZE = 300;
+const GROUP_GAP = 20;
+const NODE_SIZE = 90;
+const NODE_GAP = 10;
+
+const GROUP_POSITION = [
+  "topLeft",
+  "topCenter",
+  "topRight",
+  "centerLeft",
+  "centerCenter",
+  "centerRight",
+  "bottomLeft",
+  "bottomCenter",
+  "bottomRight",
 ];
 
+const NODE_POSITION = [
+  "topLeft",
+  "topCenter",
+  "topRight",
+  "centerLeft",
+  "centerCenter",
+  "centerRight",
+  "bottomLeft",
+  "bottomCenter",
+  "bottomRight",
+];
+
+export interface NodeData {
+  id: string;
+  title: string;
+}
+
+const initialNodes: Node<NodeData>[] = GROUP_POSITION.map((position, index) => {
+  const groupId = `${position}Group`;
+  const group: Node<NodeData> = {
+    id: groupId,
+    type: "group",
+    position: { x: (index % 3) * (GROUP_SIZE + GROUP_GAP), y: Math.floor(index / 3) * (GROUP_SIZE + GROUP_GAP) },
+    data: { id: groupId, title: "" },
+    draggable: false,
+    style: { width: GROUP_SIZE, height: GROUP_SIZE, background: "#c9c9c9" },
+  };
+
+  const nodes: Node<NodeData>[] = NODE_POSITION.map((nodePosition, index) => {
+    const id = `${position}-${nodePosition}`;
+    return {
+      id,
+      extent: "parent",
+      position: {
+        x: (index % 3) * (NODE_SIZE + NODE_GAP) + NODE_GAP / 2,
+        y: Math.floor(index / 3) * (NODE_SIZE + NODE_GAP) + NODE_GAP / 2,
+      },
+      type: "textUpdater",
+      data: { id, title: "" },
+      draggable: false,
+      parentNode: `${position}Group`,
+      style: { width: NODE_SIZE, height: NODE_SIZE },
+    };
+  });
+
+  return [group, ...nodes];
+}).flat();
+
 const MandalartProvider = ({ children }: { children: React.ReactNode }) => {
-  const [mandalartData, setMandalartData] = useState<MandalartSection[]>(initialData);
+  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+  const [rfInstance, setRfInstance] = useState<ReactFlowInstance | null>(null);
 
-  const loadMandalartData = useCallback(() => {
-    const data = localStorage.getItem(MANDAL_ART_KEY);
-    if (data) setMandalartData(JSON.parse(data));
-  }, []);
+  const save = useCallback(() => {
+    if (rfInstance) {
+      console.log("save 실행!", nodes);
+      const flow = rfInstance.toObject();
+      localStorage.setItem(FLOW_KEY, JSON.stringify(flow));
+    }
+  }, [nodes, rfInstance]);
 
-  const resetMandalartData = useCallback(() => {
-    localStorage.removeItem(MANDAL_ART_KEY);
+  const resetNodes = useCallback(() => {
+    setNodes(initialNodes);
+    localStorage.removeItem(FLOW_KEY);
+  }, [setNodes]);
 
-    setMandalartData((prevData) =>
-      produce(prevData, (draft) => {
-        draft.forEach((cell) => {
-          cell.data.forEach((data) => {
-            data.title = "";
-          });
+  const saveDebounced = useDebounce(save, 300);
+
+  const changeTitle = useCallback(
+    (id: string, title: string) => {
+      setNodes((prevNodes) => {
+        return produce(prevNodes, (draft) => {
+          const node = draft.find((node) => node.id === id);
+          if (node) {
+            node.data.title = title;
+          }
         });
-      }),
-    );
-  }, []);
-
-  const changeMandalartItemTitle = useCallback((sectionIndex: number, itemIndex: number, title: string) => {
-    setMandalartData((prevData) => {
-      const newData = produce(prevData, (draft) => {
-        draft[sectionIndex].data[itemIndex].title = title;
       });
+      saveDebounced();
+    },
+    [saveDebounced, setNodes],
+  );
 
-      localStorage.setItem(MANDAL_ART_KEY, JSON.stringify(newData));
-
-      return newData;
-    });
-  }, []);
-
+  // init
   useEffect(() => {
-    loadMandalartData();
-  }, [loadMandalartData]);
+    const flow = localStorage.getItem(FLOW_KEY);
+
+    if (flow) {
+      const flowData = JSON.parse(flow) as ReactFlowJsonObject;
+      setNodes(flowData.nodes);
+    }
+  }, [setNodes]);
 
   return (
     <MandalartDispatchContext.Provider
       value={{
-        resetMandalartData,
-        changeMandalartItemTitle,
+        setRfInstance,
+        onNodesChange,
+        save,
+        resetNodes,
+        changeTitle,
       }}
     >
       <MandalartStateContext.Provider
         value={{
-          mandalartData,
+          nodes,
         }}
       >
         {children}
