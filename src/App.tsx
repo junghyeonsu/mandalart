2c1309d9f3d41247836c846842b685246dd2f31b
정현수 <54893898+junghyeonsu@users.noreply.github.com>
2024-02-04 16:51:25 +0900

    feat: reactflow 적용 (#1)
    * feat: poc
    
    * feat: reactflow data로 교체
    
    * feat: title, description
    
    * feat: preview, related cell 자동 입력, center cell 배경
    
    * feat: 이미지 다운로드
    
    * fix: 이미지 다운로드 메서드 변경
    
    * fix: preview width, height 계산 안되던거 수정
    
    * feat: 칸 텍스트 중앙

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
+import { ReactFlowProvider } from "reactflow";
+
 import { Mandalart } from "./components/Mandalart";
 import MandalartProvider from "./contexts/MandalartContext";
 
 function App() {
   return (
     <div className="flex items-center justify-center w-full h-dvh h-vh">
-      <MandalartProvider>
-        <Mandalart />
-      </MandalartProvider>
+      <ReactFlowProvider>
+        <MandalartProvider>
+          <Mandalart />
+        </MandalartProvider>
+      </ReactFlowProvider>
     </div>
   );
 }
--- src/components/Mandalart.tsx
+++ src/components/Mandalart.tsx
@@ -1,126 +1,216 @@
-/* eslint-disable no-unused-vars */
-import { RotateCcwIcon } from "lucide-react";
-import { memo, useCallback, useMemo } from "react";
-import { useMediaQuery } from "react-responsive";
+import "reactflow/dist/style.css";
 
-import type { MandalartItem, MandalartSection } from "@/contexts/MandalartContext";
-import { Position, useMandalartDispatch, useMandalartState } from "@/contexts/MandalartContext";
+import { Label } from "@radix-ui/react-label";
+import { toPng } from "html-to-image";
+import { ImageDownIcon, RotateCcwIcon } from "lucide-react";
+import { memo } from "react";
+import ReactFlow, {
+  Background,
+  Controls,
+  getNodesBounds,
+  getViewportForBounds,
+  MiniMap,
+  useReactFlow,
+} from "reactflow";
 
-import { Dialog, DialogContent, DialogTrigger } from "./ui/dialog";
-import { Textarea } from "./ui/textarea";
+import { Drawer, DrawerContent, DrawerFooter, DrawerHeader, DrawerTrigger } from "@/components/ui/drawer";
+import type { NodeId, PositionType } from "@/contexts/MandalartContext";
+import { type NodeData, useMandalartDispatch, useMandalartState } from "@/contexts/MandalartContext";
+
+import { Input } from "./ui/input";
+
+const CELL_SIZE = 16;
+const PreviewCell = ({ cellPosition, isGroupSelected }: { cellPosition: PositionType; isGroupSelected: boolean }) => {
+  return (
+    <div
+      className={`grid grid-cols-3 w-[${CELL_SIZE * 3}px] h-[${CELL_SIZE * 3}px] ${
+        isGroupSelected && "border-2 border-gray-900"
+      }`}
+    >
+      <div
+        className={`w-[${CELL_SIZE}px] h-[${CELL_SIZE}px] border border-gray-300 ${
+          isGroupSelected && cellPosition === "topLeft" ? "bg-black" : "bg-white"
+        }`}
+      ></div>
+      <div
+        className={`w-[${CELL_SIZE}px] h-[${CELL_SIZE}px] border border-gray-300 ${
+          isGroupSelected && cellPosition === "topCenter" ? "bg-black" : "bg-white"
+        }`}
+      ></div>
+      <div
+        className={`w-[${CELL_SIZE}px] h-[${CELL_SIZE}px] border border-gray-300 ${
+          isGroupSelected && cellPosition === "topRight" ? "bg-black" : "bg-white"
+        }`}
+      ></div>
+      <div
+        className={`w-[${CELL_SIZE}px] h-[${CELL_SIZE}px] border border-gray-300 ${
+          isGroupSelected && cellPosition === "centerLeft" ? "bg-black" : "bg-white"
+        }`}
+      ></div>
+      <div
+        className={`w-[${CELL_SIZE}px] h-[${CELL_SIZE}px] border border-gray-300 ${
+          isGroupSelected && cellPosition === "centerCenter" ? "bg-black" : "bg-white"
+        }`}
+      ></div>
+      <div
+        className={`w-[${CELL_SIZE}px] h-[${CELL_SIZE}px] border border-gray-300 ${
+          isGroupSelected && cellPosition === "centerRight" ? "bg-black" : "bg-white"
+        }`}
+      ></div>
+      <div
+        className={`w-[${CELL_SIZE}px] h-[${CELL_SIZE}px] border border-gray-300 ${
+          isGroupSelected && cellPosition === "bottomLeft" ? "bg-black" : "bg-white"
+        }`}
+      ></div>
+      <div
+        className={`w-[${CELL_SIZE}px] h-[${CELL_SIZE}px] border border-gray-300 ${
+          isGroupSelected && cellPosition === "bottomCenter" ? "bg-black" : "bg-white"
+        }`}
+      ></div>
+      <div
+        className={`w-[${CELL_SIZE}px] h-[${CELL_SIZE}px] border border-gray-300 ${
+          isGroupSelected && cellPosition === "bottomRight" ? "bg-black" : "bg-white"
+        }`}
+      ></div>
+    </div>
+  );
+};
+
+const PreviewBoard = ({ id }: { id: NodeId }) => {
+  const [groupPosition, cellPosition] = id.split("-") as [PositionType, PositionType];
+
+  return (
+    <div className={`grid grid-cols-3 w-[144px] h-[144px] border rounded-sm bg-gray-100`}>
+      <PreviewCell cellPosition={cellPosition} isGroupSelected={groupPosition === "topLeft"} />
+      <PreviewCell cellPosition={cellPosition} isGroupSelected={groupPosition === "topCenter"} />
+      <PreviewCell cellPosition={cellPosition} isGroupSelected={groupPosition === "topRight"} />
+      <PreviewCell cellPosition={cellPosition} isGroupSelected={groupPosition === "centerLeft"} />
+      <PreviewCell cellPosition={cellPosition} isGroupSelected={groupPosition === "centerCenter"} />
+      <PreviewCell cellPosition={cellPosition} isGroupSelected={groupPosition === "centerRight"} />
+      <PreviewCell cellPosition={cellPosition} isGroupSelected={groupPosition === "bottomLeft"} />
+      <PreviewCell cellPosition={cellPosition} isGroupSelected={groupPosition === "bottomCenter"} />
+      <PreviewCell cellPosition={cellPosition} isGroupSelected={groupPosition === "bottomRight"} />
+    </div>
+  );
+};
+
+const CustomTextNode = ({ data }: { data: NodeData }) => {
+  const { changeData } = useMandalartDispatch();
+
+  const { id, title, description } = data;
+
+  const [, cellPosition] = id.split("-") as [PositionType, PositionType];
+  const isCenterCell = cellPosition === "centerCenter";
+
+  const onChangeTitle = (e: React.ChangeEvent<HTMLInputElement>) => {
+    changeData(id, "title", e.target.value);
+  };
+
+  const onChangeDescription = (e: React.ChangeEvent<HTMLInputElement>) => {
+    changeData(id, "description", e.target.value);
+  };
+
+  return (
+    <Drawer>
+      <DrawerTrigger asChild>
+        <div
+          className={`w-[90px] h-[90px] flex flex-col items-center justify-center border border-primary rounded-sm cursor-pointer break-all overflow-auto text-center ${
+            isCenterCell ? "bg-gray-100" : "bg-white"
+          }`}
+        >
+          <p className="text-primary font-bold text-sm">{title}</p>
+          <p className="text-muted-foreground text-xs">{description}</p>
+        </div>
+      </DrawerTrigger>
+      <DrawerContent>
+        <DrawerHeader className="flex items-center justify-center">
+          <PreviewBoard id={id} />
+        </DrawerHeader>
+        <DrawerFooter>
+          <div className="flex flex-col gap-4">
+            <div className="grid w-full gap-1.5">
+              <Label htmlFor="title">만다라트 제목</Label>
+              <Input id="title" value={title} onInput={onChangeTitle} className={`resize-none`} />
+              <p className="text-xs text-muted-foreground">핵심적인 문장을 적어주세요.</p>
+            </div>
+            <div className="grid w-full gap-1.5">
+              <Label htmlFor="title">만다라트 설명</Label>
+              <Input id="title" value={description} onInput={onChangeDescription} className={`resize-none`} />
+              <p className="text-xs text-muted-foreground">해당 만다라트에 대한 설명을 적어주세요.</p>
+            </div>
+          </div>
+        </DrawerFooter>
+      </DrawerContent>
+    </Drawer>
+  );
+};
+
+const nodeTypes = { textUpdater: CustomTextNode };
 
 const Mandalart = () => {
-  const isLarge = useMediaQuery({ query: "(min-width: 1024px)" });
-  const { mandalartData } = useMandalartState();
+  const { nodes } = useMandalartState();
+  const { onNodesChange, setRfInstance } = useMandalartDispatch();
 
   return (
     <>
       <ResetButton />
-      <div className="grid grid-cols-3 gap-2 lg:gap-4">
-        {mandalartData.map((section, sectionPosition) => {
-          return isLarge ? (
-            <Section key={sectionPosition} sectionData={section} sectionPosition={sectionPosition} modal={false} />
-          ) : (
-            <Dialog key={sectionPosition}>
-              <DialogTrigger>
-                <Section sectionData={section} sectionPosition={sectionPosition} />
-              </DialogTrigger>
-              <DialogContent className="flex flex-col items-center">
-                <ModalPreviewSection position={sectionPosition} />
-                <Section modal sectionData={section} sectionPosition={sectionPosition} />
-              </DialogContent>
-            </Dialog>
-          );
-        })}
+      <ImageDownloadButton />
+
+      <div className="w-[100vh] h-[100vh]">
+        <ReactFlow
+          minZoom={0.3}
+          maxZoom={2}
+          fitView
+          nodes={nodes}
+          onNodesChange={onNodesChange}
+          nodeTypes={nodeTypes}
+          onInit={setRfInstance}
+        >
+          <Controls />
+          <MiniMap />
+          <Background gap={20} size={1} />
+        </ReactFlow>
       </div>
     </>
   );
 };
 
-const ResetButton = memo(() => {
-  const { resetMandalartData } = useMandalartDispatch();
-  return (
-    <RotateCcwIcon onClick={resetMandalartData} className="fixed top-4 right-4 w-6 h-6 text-primary cursor-pointer" />
-  );
+function downloadImage(dataUrl: string) {
+  const a = document.createElement("a");
+
+  a.setAttribute("download", "mandalart.png");
+  a.setAttribute("href", dataUrl);
+  a.click();
+}
+
+const ImageDownloadButton = memo(() => {
+  const { getNodes } = useReactFlow();
+
+  const download = () => {
+    const nodesBounds = getNodesBounds(getNodes());
+    const transform = getViewportForBounds(nodesBounds, 1024, 768, 0.5, 2);
+
+    const viewport = document.querySelector(".react-flow__viewport") as HTMLElement;
+
+    toPng(viewport, {
+      backgroundColor: "#ffffff",
+      width: 1024,
+      height: 768,
+      style: {
+        width: `1024`,
+        height: `768`,
+        transform: `translate(${transform.x}px, ${transform.y}px) scale(${transform.zoom})`,
+      },
+    }).then(downloadImage);
+  };
+
+  return <ImageDownIcon onClick={download} className="fixed top-4 right-12 w-6 h-6 text-primary cursor-pointer z-10" />;
 });
 
-const ModalPreviewSection = ({ position }: { position: Position }) => {
+const ResetButton = memo(() => {
+  const { resetNodes } = useMandalartDispatch();
   return (
-    <div className="grid grid-cols-3 gap-1 w-16 h-16">
-      <div className={`w-4 h-4 ${position === Position.topLeft ? "bg-primary" : "bg-gray-300"}`} />
-      <div className={`w-4 h-4 ${position === Position.topCenter ? "bg-primary" : "bg-gray-300"}`} />
-      <div className={`w-4 h-4 ${position === Position.topRight ? "bg-primary" : "bg-gray-300"}`} />
-      <div className={`w-4 h-4 ${position === Position.centerLeft ? "bg-primary" : "bg-gray-300"}`} />
-      <div className={`w-4 h-4 ${position === Position.centerCenter ? "bg-primary" : "bg-gray-300"}`} />
-      <div className={`w-4 h-4 ${position === Position.centerRight ? "bg-primary" : "bg-gray-300"}`} />
-      <div className={`w-4 h-4 ${position === Position.bottomLeft ? "bg-primary" : "bg-gray-300"}`} />
-      <div className={`w-4 h-4 ${position === Position.bottomCenter ? "bg-primary" : "bg-gray-300"}`} />
-      <div className={`w-4 h-4 ${position === Position.bottomRight ? "bg-primary" : "bg-gray-300"}`} />
-    </div>
-  );
-};
-
-interface SectionProps {
-  sectionData: MandalartSection;
-  sectionPosition: Position;
-  modal?: boolean;
-}
-
-const Section = ({ sectionData, sectionPosition, modal }: SectionProps) => {
-  return (
-    <div className="grid grid-cols-3 gap-1 lg:gap-2 items-center">
-      {sectionData.data.map((item, itemPosition) => {
-        return (
-          <Item
-            key={`${sectionPosition}+${itemPosition}`}
-            item={item}
-            itemPosition={itemPosition}
-            sectionPosition={sectionPosition}
-            modal={modal}
-          />
-        );
-      })}
-    </div>
-  );
-};
-
-interface ItemProps {
-  item: MandalartItem;
-  itemPosition: Position;
-  sectionPosition: Position;
-  modal?: boolean;
-}
-
-const Item = memo(({ item, itemPosition, sectionPosition, modal }: ItemProps) => {
-  const { changeMandalartItemTitle } = useMandalartDispatch();
-
-  const isCenterItem = useMemo(() => itemPosition === Position.centerCenter, [itemPosition]);
-  const isCenterSection = useMemo(() => sectionPosition === Position.centerCenter, [sectionPosition]);
-  const isOutsideSectionCenter = useMemo(() => isCenterItem && !isCenterSection, [isCenterItem, isCenterSection]);
-  const title = useMemo(() => item.title, [item.title]);
-
-  const handleChange = useCallback(
-    (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-      changeMandalartItemTitle(sectionPosition, itemPosition, e.target.value);
-
-      if (isCenterSection) {
-        changeMandalartItemTitle(itemPosition, sectionPosition, e.target.value);
-      }
-    },
-    [changeMandalartItemTitle, isCenterSection, itemPosition, sectionPosition],
-  );
-
-  return (
-    <div key={itemPosition} className={`lg:w-20 lg:h-20 ${modal ? "w-24 h-24" : "w-10 h-10"}`}>
-      <Textarea
-        className={`resize-none text-xxs lg:text-sm text-center ${isCenterItem && "font-bold"} ${
-          isOutsideSectionCenter && "bg-gray-100"
-        }`}
-        value={title}
-        onChange={handleChange}
-        readOnly={isOutsideSectionCenter}
-      />
-    </div>
+    <RotateCcwIcon onClick={resetNodes} className="fixed top-4 right-4 w-6 h-6 text-primary cursor-pointer z-10" />
   );
 });
 
