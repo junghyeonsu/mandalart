5acdc7791026b776940674debf3a79788976dd5a
정현수 <54893898+junghyeonsu@users.noreply.github.com>
2024-02-17 01:12:28 +0900

    feat: 임시 토스트

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,37 @@
-import { ReactFlowProvider } from "reactflow";
+import { useEffect } from "react";
+
+import { useToast } from "@/components/ui/use-toast";
 
 import { Mandalart } from "./components/Mandalart";
-import MandalartProvider from "./contexts/MandalartContext";
+import { ToastAction } from "./components/ui/toast";
 
 function App() {
+  const { toast } = useToast();
+
+  // INFO: 임시로 개발중임을 알리는 토스트를 띄웁니다.
+  useEffect(() => {
+    const isDismissed = localStorage.getItem("mandalart-isDismissed");
+    if (isDismissed) return;
+    toast({
+      title: "현재는 개발 진행중입니다.",
+      description: "UI가 정상적으로 보이지 않으면 데이터를 삭제하고 다시 시도해보세요.",
+      duration: 5000,
+      action: (
+        <ToastAction
+          onClick={() => {
+            localStorage.setItem("mandalart-isDismissed", "true");
+          }}
+          altText="다시보지않기"
+        >
+          다시보지않기
+        </ToastAction>
+      ),
+    });
+  }, [toast]);
+
   return (
     <div className="flex items-center justify-center w-full h-dvh h-vh">
-      <ReactFlowProvider>
-        <MandalartProvider>
-          <Mandalart />
-        </MandalartProvider>
-      </ReactFlowProvider>
+      <Mandalart />
     </div>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,20 @@ import "./index.css";
 
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { ReactFlowProvider } from "reactflow";
+
+import { Toaster } from "@/components/ui/toaster";
 
 import App from "./App.tsx";
+import MandalartProvider from "./contexts/MandalartContext.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <ReactFlowProvider>
+      <MandalartProvider>
+        <Toaster />
+        <App />
+      </MandalartProvider>
+    </ReactFlowProvider>
   </React.StrictMode>,
 );
